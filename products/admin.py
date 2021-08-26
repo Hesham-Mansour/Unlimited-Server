f911ed88e065f75b8ed0cc3d211de03ba25152aa
Hesham-Mansour <82823017+Hesham-Mansour@users.noreply.github.com>
2021-08-26 15:41:10 -0700

    Update admin.py

--- products/admin.py
+++ products/admin.py
@@ -4,3 +4,8 @@ from django.contrib import admin
 # Register your models here.
 
 admin.site.register(Product)
+
+class ArticleAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
